aeb37cc0f55da5cd814b8c4936c9004393f0a2b8
gmugwe <ggmugwe@gmail.com>
2017-04-18 07:58:11 +0300

    correct linking

--- js/Scripts.js
+++ js/Scripts.js
@@ -1,17 +1,22 @@
-// Business (or back-end) logic:
+//front-end
+$(document).ready(function() {
+       $("#game-form").submit(function(event) {
+         var number1 = parseInt($("#pingpong").val());
 
-    var pingPongFunction = function (number) {
-      var listResult="";
-     };
-
-  // start of user interface (front-end) logic:
-
-  $(document).ready(function() {
-      $("form#add").submit(function(event) {
-        event.preventDefault();
-        var number1 = parseInt($("#add1").val());
-        var number2 = parseInt($("#add2").val());
-        var result = add(number1, number2);
-        $("#output").text(result);
-      });
-    });
+         for (var i=1; i <=number1; i++){
+             if( i % 3 ===0 && i % 5===0 ) {
+               $("#result").append("<li>pingpong</li>");
+             }
+             else if ( i % 3 === 0 ) {
+                     $("#result").append("<li>ping</li>");
+                   }
+                   else  if( i % 5 === 0 ) {
+                     $("#result").append("<li>pong</li>");
+                   }
+                   else {
+                     $("#result").append("<li>" + i +"</li>");
+                   }
+                 }
+                 event.preventDefault();
+       });
+     });
